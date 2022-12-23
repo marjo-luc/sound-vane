e4980e59fa64304695e182c69ead3aef001f85d0
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2022-12-23 20:29:52 +0100

    UI panel

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { getRelativeAQI } from "../../utils/aqi";
 import { Synth } from '../Synth';
 import "./style.css";
 
 export const Player = ({lnglat, aqi}) => {
 
+    const [isPlaying, setIsPlaying] = useState(false)
+    
     if (aqi) {
         console.log("The air quality values are: ", aqi)
         console.log("The air quality relative values are: ", getRelativeAQI(aqi))
     }
 
     return <div className="player-wrapper">
-        <h2>Sound Vane<br/>{lnglat.lng}, {lnglat.lat}<br/>15 Apr 2022</h2>
-        <Synth />
+        <div className="flex space-between">
+            <div>
+                <div>Los Angeles</div>
+                <div>USA</div>
+            </div>
+            <div>15:08</div>
+        </div>
+        {/* <h2>{lnglat.lng}, {lnglat.lat}<br/>15 Apr 2022</h2> */}
+        {aqi && 
+            <>
+                <Synth RelativeAQI={getRelativeAQI(aqi)} />
+                <div className="flex key">
+                    <div className="keyItem">
+                        <div>CO</div>
+                        <div>{aqi.co}</div>
+                    </div>
+                    <div className="keyItem">
+                        <div>HUMIDITY</div>
+                        <div>{aqi.humidity} hPa</div>
+                    </div>
+                    <div className="keyItem">
+                        <div>O3</div>
+                        <div>{aqi.o3}</div>
+                    </div>
+                    <div className="keyItem">
+                        <div>PARTICLES</div>
+                        <div>{aqi.pm25} PM (2.5)</div>
+                    </div>
+                </div>
+            </>
+        }
     </div>
 }
