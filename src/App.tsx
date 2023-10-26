05fc5da21fefe9299d60a3947db740db97d9c56c
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2023-10-26 14:48:04 +0200

    static heatmap

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-import React, { useState } from 'react';
-import { Header } from './components/Header';
-import { MapBox } from './components/Map';
-import { Crosshair } from './components/Crosshair';
-import { LngLatLike } from 'mapbox-gl';
-import { MainMenu } from './components/Menus';
-import { About } from './components/About';
+import React, { useState } from "react"
+import { Header } from "./components/Header"
+import { MapBox } from "./components/Map"
+import { Crosshair } from "./components/Crosshair"
+import { LngLatLike } from "mapbox-gl"
+import { MainMenu } from "./components/Menus"
+import { About } from "./components/About"
 
 export const App = () => {
+  const [lnglat, setLngLat] = useState<LngLatLike>({ lng: -70.9, lat: 42.35 })
+  const [aqi, setAQI] = useState()
 
-  const [lnglat, setLngLat] = useState<LngLatLike>({lng: -70.9, lat: 42.35});
-  const [aqi, setAQI] = useState();
-
-  const [userHasInteracted, setUserHasInteracted] = useState(false);
-  const [toggleAbout, setToggleAbout] = useState(true);
+  const [userHasInteracted, setUserHasInteracted] = useState(false)
+  const [toggleAbout, setToggleAbout] = useState(true)
 
   return (
     <>
       <Header />
       {toggleAbout && (
-        <About 
+        <About
           toggleAbout
           setToggleAbout={setToggleAbout}
-          setUserHasInteracted={setUserHasInteracted}  
+          setUserHasInteracted={setUserHasInteracted}
         />
       )}
-      <MapBox 
-        lnglat={lnglat} 
-        setLngLat={setLngLat} 
-        setAQI={setAQI} 
+      <MapBox
+        lnglat={lnglat}
+        setLngLat={setLngLat}
+        setAQI={setAQI}
         userHasInteracted={userHasInteracted}
       />
       <MainMenu lnglat={lnglat} aqi={aqi} />
       <Crosshair />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,44 +1,102 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import "./style.css";
-import { getAQIValues } from '../../apis/aqi';
+import React, { useRef, useEffect, useState } from "react"
+import mapboxgl from "mapbox-gl"
+import "./style.css"
+import { getAQIValues } from "../../apis/aqi"
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX
 
 export const MapBox = ({ lnglat, setLngLat, setAQI, userHasInteracted }) => {
-    const mapContainer = useRef(null);
-    const [zoom, setZoom] = useState(4);
+  const mapContainer = useRef(null)
+  const [zoom, setZoom] = useState(4)
 
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lnglat.lng, lnglat.lat],
-            zoom: zoom
-        });
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lnglat.lng, lnglat.lat],
+      zoom: zoom,
+    })
 
-        // Add navigation control (the +/- zoom buttons)
-        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "top-right")
 
-        map.on('dragend', () => {
-            setLngLat({ lat: map.getCenter().lat.toFixed(2), lng: map.getCenter().lng.toFixed(2) })
-            setZoom(map.getZoom());
-        });
+    map.on("dragend", () => {
+      setLngLat({
+        lat: map.getCenter().lat.toFixed(2),
+        lng: map.getCenter().lng.toFixed(2),
+      })
+      setZoom(map.getZoom())
+    })
 
-        // Clean up on unmount
-        return () => map.remove();
-    }, []);
+    map.on("load", () => {
+      map.addSource("aq", {
+        type: "geojson",
+        data: "aq.geojson",
+      })
 
+      map.addLayer({
+        id: "aq-layer",
+        type: "heatmap",
+        source: "aq",
+        paint: {
+          // Increase the heatmap weight based on frequency and property magnitude
+          "heatmap-weight": [
+            "interpolate",
+            ["linear"],
+            ["get", "mag"],
+            0,
+            0,
+            6,
+            1,
+          ],
+          // Increase the heatmap color weight weight by zoom level
+          // heatmap-intensity is a multiplier on top of heatmap-weight
+          "heatmap-intensity": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            0.5,
+            0.5,
+            0.1,
+          ],
+          // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+          // Begin color ramp at 0-stop with a 0-transparancy color
+          // to create a blur-like effect.
+          "heatmap-color": [
+            "interpolate",
+            ["linear"],
+            ["heatmap-density"],
+            0,
+            "rgba(33,102,172,0)",
+            0.5,
+            "lightgreen",
+            0.75,
+            "yellow",
+            1,
+            "red",
+          ],
+          // Adjust the heatmap radius by zoom level
+          "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+          // Transition from heatmap to circle layer by zoom level
+          "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
+        },
+      })
+    })
 
-    useEffect(() => {
-        getAQIValues(lnglat).then((result) => {
-            userHasInteracted && setAQI(result)
-        })
-    }, [lnglat, setAQI, userHasInteracted]);
+    // Clean up on unmount
+    return () => map.remove()
+  }, [])
 
-    return (
-        <div>
-            <div ref={mapContainer} className="map-container" />
-        </div>
-    );
+  useEffect(() => {
+    getAQIValues(lnglat).then((result) => {
+      userHasInteracted && setAQI(result)
+    })
+  }, [lnglat, setAQI, userHasInteracted])
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  )
 }
